41994857d75b3ccd7673fb955a5a742bf59bdca8
Ermina_Joseph <ermina.joseph@gmail.com>
2019-11-04 16:58:25 -0500

    entering initial jquery code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,5 +12,50 @@ $(document).ready(function() {
     var totalScore = 0;
     $("#total-score").text(totalScore);
 
+    var ruby = Math.floor((Math.random() * 12) + 1);
+    console.log(ruby);
 
+    $("#ruby").on("click", function() {
+        totalScore = totalScore + ruby;
+    });
+
+    var sapphire = Math.floor((Math.random() * 12) + 1);
+    console.log(sapphire);
+    $("#sapphire").on("click", function() {
+        totalScore = totalScore + sapphire;
+    });
+
+
+    var amethyst = Math.floor((Math.random() * 12) + 1);
+    console.log(amethyst);
+    $("#amethyst").on("click", function() {
+        totalScore = totalScore + amethyst; 
+    });
+
+
+    var garnet = Math.floor((Math.random() * 12) + 1);
+    console.log(garnet);
+    $("#garnet").on("click", function() { 
+        totalScore = totalScore + garnet;
+    });
+
+    if (totalScore === randomNumber) {
+        wins++;
+        randomNumber = Math.floor((Math.random() * 120) + 19);
+        ruby = Math.floor((Math.random() * 12) + 1);
+        sapphire = Math.floor((Math.random() * 12) + 1);
+        amethyst = Math.floor((Math.random() * 12) + 1);
+        garnet = Math.floor((Math.random() * 12) + 1);
+        totalScore = 0;
+    } else if (totalScore > randomNumber) {
+        losses++;
+        randomNumber = Math.floor((Math.random() * 120) + 19);
+        ruby = Math.floor((Math.random() * 12) + 1);
+        sapphire = Math.floor((Math.random() * 12) + 1);
+        amethyst = Math.floor((Math.random() * 12) + 1);
+        garnet = Math.floor((Math.random() * 12) + 1);
+        totalScore = 0;
+    }
+
+    
 });
