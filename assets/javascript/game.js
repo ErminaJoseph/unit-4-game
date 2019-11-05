166e86484165c3384a9719429acf105ee157f3b5
Ermina_Joseph <ermina.joseph@gmail.com>
2019-11-04 19:01:57 -0500

    updated code so that the total score updates when gems are clicked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,14 +14,16 @@ $(document).ready(function() {
 
     var ruby = Math.floor((Math.random() * 12) + 1);
     console.log(ruby);
-    $("#ruby").on("click", function(event) {
+    $("#ruby").on("click", function() {
         totalScore = totalScore + ruby;
+        $("#total-score").html(totalScore);
     });
 
     var sapphire = Math.floor((Math.random() * 12) + 1);
     console.log(sapphire);
     $("#sapphire").on("click", function() {
         totalScore = totalScore + sapphire;
+        $("#total-score").html(totalScore);
     });
 
 
@@ -29,6 +31,7 @@ $(document).ready(function() {
     console.log(amethyst);
     $("#amethyst").on("click", function() {
         totalScore = totalScore + amethyst; 
+        $("#total-score").html(totalScore);
     });
 
 
@@ -36,6 +39,7 @@ $(document).ready(function() {
     console.log(garnet);
     $("#garnet").on("click", function() { 
         totalScore = totalScore + garnet;
+        $("#total-score").html(totalScore);
     });
 
     if (totalScore === randomNumber) {
@@ -46,15 +50,19 @@ $(document).ready(function() {
         amethyst = Math.floor((Math.random() * 12) + 1);
         garnet = Math.floor((Math.random() * 12) + 1);
         totalScore = 0;
+        $("#total-score").html(totalScore);
+        $("#wins").html(wins);
     } else if (totalScore > randomNumber) {
         losses++;
+        console.log(losses);
         randomNumber = Math.floor((Math.random() * 120) + 19);
         ruby = Math.floor((Math.random() * 12) + 1);
         sapphire = Math.floor((Math.random() * 12) + 1);
         amethyst = Math.floor((Math.random() * 12) + 1);
         garnet = Math.floor((Math.random() * 12) + 1);
         totalScore = 0;
+        $("#total-score").html(totalScore);
+        $("#losses").html(losses);
     }
 
-    
 });
